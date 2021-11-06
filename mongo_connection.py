97661315f46fc1f803fe1eb9db927be3259a29aa
ar2rworld <60049241+ar2rworld@users.noreply.github.com>
2021-11-05 21:19:15 -0700

    ping mongo then client created

--- mongo_connection.py
+++ mongo_connection.py
@@ -1,5 +1,17 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, results
+from pymongo.errors import ConnectionFailure
 
 def get_db(mongo_obj: object):
   connection_string = f'mongodb://{mongo_obj["host"]}:{mongo_obj["port"]}'
-  return MongoClient(connection_string)[mongo_obj["dbname"]]
+  client = MongoClient(connection_string)
+  try:
+    if client[mongo_obj["dbname"]].command('ping'):
+      return client
+  except ConnectionFailure:
+    print('mongo ConnectionFailure')
+    raise ConnectionFailure    
+
+def test_get_db():
+  client = get_db({'host': 'localhost', 'port': '27027',  'dbname': 'nemobot'})
+  print(client)
+#test_get_db()
