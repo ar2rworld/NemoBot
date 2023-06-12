33fbd11f94576c296b107fa51512ac01ec42e33e
Artur Linnik <ar2r.world@gmail.com>
2023-06-12 15:01:51 -0400

    missing fields check and async

--- src/handlers/echoHandler.py
+++ src/handlers/echoHandler.py
@@ -1,18 +1,23 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
 from src.handlers.room204 import osuzhdau
 from src.handlers.stats import save_conversation
 from src.utils.echo import echo
 
 
-def echoHandler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    echo(update, context)
+async def echo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await echo(update, context)
 
     save_conversation(context, update.message)
 
-    osuzhdau(update, context)
+    await osuzhdau(update, context)
 
     bot_data = context.application.bot_data
+    if update.message is None or update.message.from_user is None:
+        raise ValueError("Missing message or chat or from_user")
     try:
-        userId = update.message.from_user.id
-        bot_data["echoHandlers"][userId](update, context)
+        user_id = update.message.from_user.id
+        bot_data["echoHandlers"][user_id](update, context)
     except KeyError:
         pass
