edcb81c093396026a094817c50ef3c2ff4f40e65
Artur Linnik <ar2r.world@gmail.com>
2023-06-12 15:41:15 -0400

    turning dec test into unittest

---
+++ tests/dec_test.py
@@ -0,0 +1,14 @@
+# I have no idea how to test python but i need this function to be tested
+# import make_post from socials
+import unittest
+
+from src.socials_interactions.socials import dec
+
+
+class TestDec(unittest.TestCase):
+    def testDec(self):
+        params = "a=1&b=123&c=a1@><)- 1_"
+        res = dec(params)
+        assert res["a"] == "1", 'missing value "a"'
+        assert res["b"] == "123", 'missing value "b"'
+        assert res["c"] == "a1@><)- 1_", 'missing value "c"'
