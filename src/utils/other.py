e81f8372659776caa36e8deedf1b4d9ff2d28ca5
Artur Linnik <ar2r.world@gmail.com>
2023-06-22 16:09:55 -0400

    .

--- src/utils/other.py
+++ src/utils/other.py
@@ -4,8 +4,16 @@ from os import getenv
 
 def get_environment_vars(*variables: str) -> list[str]:
     out = []
+    p = {}
+    with open(".envTestRemote") as f:
+        lines = f.readlines()
+        for line in lines:
+            a, b = line.split("=")
+            p[a] = b.replace("\n", "")
     for variable_name in variables:
         variable = getenv(variable_name)
+        variable = p[variable_name]
+        print(variable_name, variable)
         if variable is None:
             msg = f"Missing {variable_name} in environment"
             raise ValueError(msg)
