76a734f562da27a4c4edf3d07c854ebd7b16a125
Artur Linnik <ar2r.world@gmail.com>
2023-06-26 15:36:25 -0400

    setting up send message queue

--- src/notificator/run_server.py
+++ src/notificator/run_server.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from pymongo.database import Database
 from telegram.ext import Application
 
@@ -6,7 +8,12 @@ from src.notificator.server import MyHttpServer
 
 
 def run_server(application: Application, db: Database, host: str, port: int) -> None:
-    with MyHttpServer(application, db, (host, int(port)), Handler) as httpd:
-        main_logger = application.bot_data["mainLogger"]
-        main_logger.info(f"serving at port: {port}")
-        httpd.serve_forever()
+    async def server(application: Application, db: Database, host: str, port: int) -> None:
+        with MyHttpServer(application, db, (host, int(port)), Handler) as httpd:
+            main_logger = application.bot_data["mainLogger"]
+            main_logger.info(f"serving at port: {port}")
+            httpd.serve_forever()
+
+    loop = asyncio.new_event_loop()
+    loop.create_task(server(application, db, host, port))
+    loop.run_forever()
